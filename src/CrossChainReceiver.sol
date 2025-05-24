592863ccb6535efa08497f8c5f456812e00f3022
SAYEED <90544352+msayeeduddina@users.noreply.github.com>
2025-05-24 15:10:01 +0530

    wormhole IERC20 latest path

--- src/CrossChainReceiver.sol
+++ src/CrossChainReceiver.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
-import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
+import "lib/wormhole-solidity-sdk/src/interfaces/token/IERC20.sol";
 
 contract CrossChainReceiver is TokenReceiver {
     // The wormhole relayer and registeredSenders are inherited from the Base.sol contract.
