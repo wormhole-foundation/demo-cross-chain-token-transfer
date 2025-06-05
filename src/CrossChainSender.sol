23a85d7aed58f6bb93e4736590bc046fb4ea1f73
SAYEED <90544352+msayeeduddina@users.noreply.github.com>
2025-06-05 20:41:51 +0530

    wormhole IERC20 latest path (#9)

--- src/CrossChainSender.sol
+++ src/CrossChainSender.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
-import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
+import "lib/wormhole-solidity-sdk/src/interfaces/token/IERC20.sol";
 
 contract CrossChainSender is TokenSender {
     uint256 constant GAS_LIMIT = 250_000;
